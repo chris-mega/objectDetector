c34735d23bedc7f00566f654722f1d4243b607b2
Chris Melendez <chris.melendezg@gmail.com>
2019-09-29 00:31:31 -0500

    ball training and start arrow detection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.vision import Vision, MAIN_WINDOW_NAME, DEBUG
-from src.object_detection import ball_tracker
+from src.object_detection import ball_tracker, ball_tracker_cascade, marker_detect
 import cv2 as cv
 from src.yaml_parser import Parser
 import os
@@ -13,6 +13,12 @@ if __name__ == '__main__':
     vision = Vision(object_to_read['ball'])
     ball_cascade = None
 
+    folder = os.getcwd()
+    folder = os.path.join(folder, 'src')
+    forward = cv.imread('{}/forward.png'.format(folder), 0)
+    left = cv.imread('{}/left.png'.format(folder), 0)
+    right = cv.imread('{}/right.png'.format(folder), 0)
+
     if with_cascade:
         path = os.getcwd()
         path = os.path.join(path, 'tools/data/cascade.xml')
@@ -22,8 +28,22 @@ if __name__ == '__main__':
         vision.read_camera()
         vision.create_hsv()
 
-        ball_tracker(vision.frame, vision.debug, ball_cascade)
+        # ball with color
+        x_circle, y_circle, radius = ball_tracker(vision.frame, vision.debug, ball_cascade)
+        # ball with cascade
+        ball_t_cascade = ball_tracker_cascade(vision.frame, ball_cascade)
+        # arrow detect
+        marker_detect(vision.frame, forward, left, right)
 
+        # draw color balls
+        if x_circle > -1 and y_circle > -1 and radius > -1:
+            cv.circle(vision.frame, (int(x_circle), int(y_circle)), int(radius), (0,255,255), 2)
+
+        # draw cascade balls
+        if ball_t_cascade is not None:
+            for (x, y, w, h) in ball_t_cascade:
+                cv.rectangle(vision.frame, (x, y), (x+w, y+h), (255,0,0), 2)
+        
         cv.imshow(MAIN_WINDOW_NAME, vision.frame)
         cv.imshow(DEBUG, vision.debug)
         # cv.imshow('hsv', vision.hsv)
--- src/object_detection.py
+++ src/object_detection.py
@@ -2,18 +2,22 @@ import cv2 as cv
 import numpy as np
 import os
 
-def ball_tracker(frame, debug, cascade=None):
+def ball_tracker_cascade(frame, cascade):
     inp = frame.copy()
 
     gray = cv.cvtColor(inp, cv.COLOR_BGR2GRAY)
 
+
     if cascade is not None:
         balls = cascade.detectMultiScale(gray)
 
-        for (x, y, w, h) in balls:
-            cv.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+        return balls
 
-    
+    return None
+        
+
+
+def ball_tracker(frame, debug, cascade=None):
     # with color
     _, thresh = cv.threshold(debug.copy(), 127, 255, 0)
     _, contours, _ = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -38,10 +42,7 @@ def ball_tracker(frame, debug, cascade=None):
         if c_size > 0:
             center = (int(M['m10'] / c_size), int(M['m01'] / c_size))
 
-            if radius > 5:
-                cv.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 2)
-                cv.circle(frame, center, 5, (0,0,255), 1)
-            else:
+            if radius <= 5:
                 x, y, radius = -1, -1, -1
         else:
             x, y, radius = -1, -1, -1
@@ -59,5 +60,91 @@ def ball_tracker(frame, debug, cascade=None):
 
     return x, y, radius
 
+
+def marker_detect(frame, forward, left, right):
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # edges = cv.Canny(gray, 50, 120)
+    # min_line_length = 100
+    # max_line_gap = 50
+    # lines = cv.HoughLinesP(edges, 1, np.pi/180, 100, min_line_length, max_line_gap)
+
+    # inp = frame.copy()
+    # if lines is not None:
+    #     for x1, y1, x2, y2 in lines[0]:
+    #         cv.line(inp, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    # cv.imshow('edges', edges)
+    # cv.imshow('lines', inp)
+
+    gray = cv.blur(gray, (3,3))
+    _, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+
+
+    edged = cv.Canny(gray, 30, 200)
+
+    # cv.imshow('canny',edged)
+
+    img, contours, hier = cv.findContours(edged, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+
+    posible_arrow = None
+    area = -1
+
+    for cnt in contours:
+        epsilon = 0.01 * cv.arcLength(cnt, True)
+        approx = cv.approxPolyDP(cnt, epsilon, True)
+
+        if len(approx) == 4:
+            this_area = cv.contourArea(cnt)
+            if area < this_area:
+                area = this_area
+                posible_arrow = approx.copy()
+
+    if posible_arrow is not None and area > 1000:
+        pts1 = np.zeros(shape=(4,2))
+
+        for i, p in enumerate(posible_arrow):
+            pts1[i] = p[0]
+
+        pts1.view('i8,i8').sort(order=['f1'], axis=0)
+
+        change_to = np.zeros(shape=(4,2))
+
+        if pts1[0][0] < pts1[1][0]:
+            change_to[0] = [0,0]
+            change_to[1] = [400,0]
+        else:
+            change_to[0] = [400,0]
+            change_to[1] = [0,0]
+        
+        if pts1[2][0] < pts1[3][0]:
+            change_to[2] = [0,400]
+            change_to[3] = [400,400]
+        else:
+            change_to[2] = [400,400]
+            change_to[3] = [0,400]
+
+
+        M = cv.getPerspectiveTransform(np.float32(pts1), np.float32(change_to))
+
+        dst = cv.warpPerspective(frame, M, (400, 400))
+
+        cv.imshow('dst', dst)        
+
+        orb = cv.ORB_create()
+
+        kp1, des1 = orb.detectAndCompute(forward, None)
+        kp2, des2 = orb.detectAndCompute(dst, None)
+
+        bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+
+        matches = bf.match(des1, des2)
+
+        if len(matches) > 4:
+            print('forward')
+        else:
+            print('nop')
+
+        cv.drawContours(frame, [posible_arrow], 0, (0,255,0), 2)
 
 
